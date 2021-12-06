a20aef05a30486b278f9dc977bf5019d43586c50
ChrisKader <chris.kader@gmail.com>
2021-12-06 18:17:32 +0000

    Initial

--- .devcontainer/scripts/postCreate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-npm install -g typescript@4.4.3
-npm install -g yo generator-code
-git config --global user.name "Chris Kader"
-git config --global user.email chris.kader@gmail.com
-git config --global pull.rebase false
